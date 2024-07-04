15a7f551e277dbd8e1b1791db0df7fa23b1b87ed
Kilerd Chan <blove694@gmail.com>
2024-07-05 00:21:18 +0800

    enable to fetch linked domain

--- conservator_macro/src/sql.rs
+++ conservator_macro/src/sql.rs
@@ -1,11 +1,34 @@
+use std::collections::HashSet;
 use std::str::FromStr;
 
 use itertools::Itertools;
 use proc_macro2::Span;
 use quote::{format_ident, quote};
+use regex::Regex;
 use strum::EnumString;
 use syn::spanned::Spanned;
-use syn::{parse2, Expr, FnArg, Ident, ItemFn, Lit, Pat, Stmt};
+use syn::{
+    parse2, AngleBracketedGenericArguments, Expr, FnArg, Ident, ItemFn, Lit, Pat, PathArguments,
+    ReturnType, Stmt, Type,
+};
+
+fn extract_inner_type<'a>(ty: &'a Type, wrapper: &'a str) -> Option<&'a Type> {
+    if let Type::Path(syn::TypePath { qself: None, path }) = ty {
+        if let Some(segment) = path.segments.last() {
+            if segment.ident == wrapper {
+                if let PathArguments::AngleBracketed(AngleBracketedGenericArguments {
+                    args, ..
+                }) = &segment.arguments
+                {
+                    if let Some(syn::GenericArgument::Type(inner_type)) = args.first() {
+                        return Some(inner_type);
+                    }
+                }
+            }
+        }
+    }
+    None
+}
 
 #[derive(Debug, EnumString)]
 #[strum(serialize_all = "snake_case")]
@@ -18,7 +41,12 @@ enum Action {
 }
 
 impl Action {
-    fn build_sqlx_query(&self, fields: &[String], sql: String) -> proc_macro2::TokenStream {
+    fn build_sqlx_query(
+        &self,
+        fields: &[String],
+        fetch_model: &proc_macro2::TokenStream,
+        sql: String,
+    ) -> proc_macro2::TokenStream {
         let fields = fields
             .iter()
             .filter(|&field| !field.eq("executor"))
@@ -28,7 +56,7 @@ impl Action {
             Action::Fetch => {
                 if cfg!(debug_assertions) {
                     quote! {
-                        ::sqlx::query_as!(Self, #sql, #(#fields,)*)
+                        ::sqlx::query_as!(#fetch_model, #sql, #(#fields,)*)
                             .fetch_one(executor)
                             .await
                     }
@@ -44,13 +72,13 @@ impl Action {
             Action::Exists => {
                 if cfg!(debug_assertions) {
                     quote! {
-                        Ok(::sqlx::query_as!((bool, ), #sql, #(#fields,)*)
+                        Ok(::sqlx::query_as!(#fetch_model, #sql, #(#fields,)*)
                             .fetch_one(executor)
                             .await?.0)
                     }
                 } else {
                     quote! {
-                        Ok(::sqlx::query_as::<_, (bool, )>(#sql)
+                        Ok(::sqlx::query_as::<_, #fetch_model>(#sql)
                         #(.bind(#fields))*
                         .fetch_one(executor)
                         .await?.0)
@@ -60,7 +88,7 @@ impl Action {
             Action::Find => {
                 if cfg!(debug_assertions) {
                     quote! {
-                        ::sqlx::query_as!(Self, #sql, #(#fields,)*)
+                        ::sqlx::query_as!(#fetch_model, #sql, #(#fields,)*)
                             .fetch_optional(executor)
                             .await
                     }
@@ -76,7 +104,7 @@ impl Action {
             Action::FetchAll => {
                 if cfg!(debug_assertions) {
                     quote! {
-                        ::sqlx::query_as!(Self, #sql, #(#fields,)*)
+                        ::sqlx::query_as!(#fetch_model, #sql, #(#fields,)*)
                             .fetch_all(executor)
                             .await
                     }
@@ -92,7 +120,7 @@ impl Action {
             Action::Execute => {
                 if cfg!(debug_assertions) {
                     quote! {
-                        ::sqlx::query_as!((i32,), #sql, #(#fields,)*)
+                        ::sqlx::query_as!(#fetch_model, #sql, #(#fields,)*)
                             .execute(executor)
                             .await?;
                         Ok(())
@@ -107,6 +135,35 @@ impl Action {
                     }
                 }
             }
+        }
+    }
+
+    fn extract_and_build_ret_type(
+        &self,
+        ident: &ReturnType,
+    ) -> Result<(proc_macro2::TokenStream, proc_macro2::TokenStream), (Span, &'static str)> {
+        let span = ident.span();
+        match ident {
+            ReturnType::Default => {
+                return Err((span, "default return type does not support"));
+            }
+            ReturnType::Type(_, inner) => match self {
+                Action::Fetch => Ok((quote! {#inner}, quote! { #inner })),
+                Action::Exists => Ok((quote! {(i32, )}, quote! { bool })),
+                Action::Find => {
+                    let Some(inner_type) = extract_inner_type(inner, "Option") else {
+                        return Err((span, "find method need a option type"));
+                    };
+                    Ok((quote! {#inner_type}, quote! { #inner }))
+                }
+                Action::FetchAll => {
+                    let Some(inner_type) = extract_inner_type(inner, "Vec") else {
+                        return Err((span, "fetchall method need a vec type"));
+                    };
+                    Ok((quote! {#inner_type}, quote! { #inner }))
+                }
+                Action::Execute => Ok((quote! { (i32, ) }, quote! { () })),
+            },
         }
     }
 }
@@ -142,7 +199,7 @@ pub(crate) fn handler(
         .collect();
     let output = &method.sig.output;
 
-    dbg!(&output);
+    let (fetch_model, return_type) = action.extract_and_build_ret_type(&output)?;
     let body = &method.block;
     let body: Vec<proc_macro2::TokenStream> = body
         .stmts
@@ -152,10 +209,19 @@ pub(crate) fn handler(
             Stmt::Expr(Expr::Lit(expr_lit)) => match &expr_lit.lit {
                 Lit::Str(lit_str) => {
                     let mut sql = lit_str.value();
-                    fields.iter().enumerate().for_each(|(idx, field)| {
+                    let re = Regex::new(r":\w+").unwrap();
+                    let matches: HashSet<&str> =
+                        re.find_iter(&sql).map(|mat| mat.as_str()).collect();
+                    let matched_fields = matches
+                        .into_iter()
+                        .map(|it| it.strip_prefix(':').unwrap().to_string())
+                        .collect_vec();
+
+                    matched_fields.iter().enumerate().for_each(|(idx, field)| {
                         sql = sql.replace(&format!(":{}", field), &format!("${}", idx + 1));
                     });
-                    let query_stmt = action.build_sqlx_query(&fields[..], sql);
+                    let query_stmt =
+                        action.build_sqlx_query(&matched_fields[..], &fetch_model, sql);
                     quote!( #query_stmt)
                 }
                 _ => {
@@ -166,11 +232,13 @@ pub(crate) fn handler(
         })
         .collect();
 
-    Ok(quote! {
-        #vis async fn #ident<'e, 'c: 'e, E: 'e + ::sqlx::Executor<'c, Database=::sqlx::Postgres>>(#inputs, executor: E) #output {
+    let ret = quote! {
+        #vis async fn #ident<'e, 'c: 'e, E: 'e + ::sqlx::Executor<'c, Database=::sqlx::Postgres>>(#inputs, executor: E) -> Result<#return_type, ::sqlx::Error> {
             #(#body )*
         }
-    })
+    };
+    println!("{}", &ret);
+    Ok(ret)
 }
 
 #[cfg(test)]
@@ -180,9 +248,9 @@ mod test {
     #[test]
     fn should_generate_fetch_sql_function() {
         use quote::quote;
-        let args = quote! { fetch };
+        let args = quote! { find };
         let input = quote! {
-            pub async fn find_user<E>(email: &str, executor: E) -> Result<Option<UserEntity>, ::sqlx::Error> {
+            pub async fn find_user(email: &str) -> Option<UserEntity> {
                 "select * from users where email = :email"
             }
         };
@@ -194,6 +262,35 @@ mod test {
             ) -> Result<Option<UserEntity>, ::sqlx::Error> {
                 ::sqlx::query_as("select * from users where email = $1")
                     .bind(email)
+                    .fetch_one(executor)
+                    .await
+            }
+        };
+        assert_eq!(
+            expected.to_string(),
+            handler(args, input).unwrap().to_string()
+        );
+    }
+
+    #[test]
+    fn should_generate_for_linked_domain() {
+        use quote::quote;
+        let args = quote! { find };
+        let input = quote! {
+            pub async fn find_user(&self) -> Option<UserEntity> {
+                let id = self.id;
+                "select * from users where email = :id"
+            }
+        };
+
+        let expected = quote! {
+            pub async fn find_user<'e, 'c: 'e, E: 'e + ::sqlx::Executor<'c, Database = ::sqlx::Postgres>>(
+                &self,
+                executor: E
+            ) -> Result<Option<UserEntity>, ::sqlx::Error> {
+                 let id = self.id;
+                ::sqlx::query_as("select * from users where email = $1")
+                    .bind(id)
                     .fetch_one(executor)
                     .await
             }
